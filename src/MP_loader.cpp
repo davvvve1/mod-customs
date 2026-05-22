b03c559c14aeccf666134a3ae4dbb165cf175159
monakaibrahim-cmyk <monakaibrahim@gmail.com>
2026-05-22 18:38:41 +0800

    fixed name for module export
    Signed-off-by: monakaibrahim-cmyk <monakaibrahim@gmail.com>

--- src/MP_loader.cpp
+++ src/MP_loader.cpp
@@ -8,7 +8,7 @@ void SC_addCustomsScripts();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addskeleton_moduleScripts()
+void Addmod_customsScripts()
 {
     SC_addCustomsScripts();
 }
