4206062d44273997030690904d95555d7bf0715c
monakaibrahim-cmyk <monakaibrahim@gmail.com>
2026-05-24 16:27:24 +0800

    Fix include paths in Customs.h by removing leading slashes
    Signed-off-by: monakaibrahim-cmyk <monakaibrahim@gmail.com>

--- src/Customs.h
+++ src/Customs.h
@@ -1,9 +1,9 @@
 #ifndef MODULE_CUSTOM_H
 #define MODULE_CUSTOM_H
 
-#include "/Quests/52/Quest52.h"
-#include "/Events/SeasonOfDiscovery/sod.h"
-#include "/Events/DungeonRespawn/DungeonRespawn.h"
-#include "/Items/Hearthstone/Hearthstone.h"
+#include "Quests/52/Quest52.h"
+#include "Events/SeasonOfDiscovery/sod.h"
+#include "Events/DungeonRespawn/DungeonRespawn.h"
+#include "Items/Hearthstone/Hearthstone.h"
 
 #endif // MODULE_CUSTOM_H
